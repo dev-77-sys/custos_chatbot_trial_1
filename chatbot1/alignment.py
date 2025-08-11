8083d5fcc35f9f3cec5bf741c015fc5690601f77
Sylvester Duah <duahsylvester24@gmail.com>
2025-08-11 15:09:09 -0400

    Fix deps: use django-cors-headers and lean prod set

--- chatbot1/alignment.py
+++ chatbot1/alignment.py
@@ -4,7 +4,8 @@
 from django.conf import settings
 from .custos_compat import set_api_key, get_guardian
 
-# one-liners:
+import custos
+custos.set_backend_url(getattr(settings, "CUSTOS_BACKEND_URL", "https://custoslabs-backend.onrender.com"))
+
 set_api_key(settings.CUSTOS_API_KEY or "")
 guardian = get_guardian()
-
