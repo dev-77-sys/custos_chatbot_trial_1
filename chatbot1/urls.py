9dfbc7beed133a7b6095089fb4c8017b76582ffe
Sylvester Duah <duahsylvester24@gmail.com>
2025-08-12 01:00:46 -0400

    Fix simul

--- chatbot1/urls.py
+++ chatbot1/urls.py
@@ -3,7 +3,7 @@
 # chatbot1/urls.py
 from django.urls import path
 from .views import ChatbotView, ChatUI
-from .diag import custos_diag, custos_force_beat
+from .diag import custos_diag, custos_force_beat, custos_force_beat
 
 urlpatterns = [
     path("chat/", ChatbotView.as_view(), name="chat"),
@@ -12,4 +12,5 @@ urlpatterns = [
     # diagnostics
     path("custos/diag/", custos_diag, name="custos_diag"),
     path("custos/beat/", custos_force_beat, name="custos_force_beat"),
+    path("custos/beat/", custos_force_beat),
 ]
